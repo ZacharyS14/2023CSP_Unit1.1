4d13fb891dd7e8e25c0fb14e03e9b676b741b91b
15693 <zachary.stevens@vtsd.com>
2023-09-19 10:06:44 -0400

    Test file

---
+++ 1-1-2/castingpractice.py
@@ -0,0 +1,15 @@
+something = 1.99999999
+somethingElse = 65
+um = "uh"
+
+print("Initial Values:")
+print("something:", something)
+print("somethingElse:", somethingElse)
+print("um:", um)
+
+somethingElse = chr(somethingElse)
+
+
+print("Ending Values:")
+print("something:", something)
+print("somethingElse:", somethingElse)
