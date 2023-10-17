c3ae40fbb0b1dea08c13984fc4a3d8c26621eea1
15693 <zachary.stevens@vtsd.com>
2023-10-17 08:18:51 -0400

    Test file

---
+++ 1-1-6/step33.py
@@ -0,0 +1,55 @@
+#   a116_ladybug.py
+import turtle as trtl
+
+# create ladybug head
+ladybug = trtl.Turtle()
+ladybug.pensize(40)
+ladybug.circle(5)
+
+# and body
+ladybug.penup()
+ladybug.goto(0, -55)
+ladybug.color("red")
+ladybug.pendown()
+ladybug.pensize(40)
+ladybug.circle(20)
+ladybug.setheading(270)
+ladybug.color("black")
+ladybug.penup()
+ladybug.goto(0, 5)
+ladybug.pensize(2)
+ladybug.pendown()
+ladybug.forward(75)
+
+# config dots
+num_dots = 1
+xpos = -20
+ypos = -55
+ladybug.pensize(10)
+
+# draw two sets of dots
+if (num_dots <= 4):
+  ladybug.penup()
+  ladybug.goto(xpos, ypos)
+  ladybug.pendown()
+  ladybug.circle(3)
+  ladybug.penup()
+  ladybug.goto(xpos + 30, ypos + 20)
+  ladybug.pendown()
+  ladybug.circle(2)
+
+  # position next dots
+if (num_dots <= 4):
+ladybug.penup()
+ladybug.goto(xpos, ypos)
+ladybug.pendown()
+ladybug.circle(3)
+ladybug.penup()
+ladybug.goto(xpos + 60, ypos + 30)
+ladybug.pendown()
+ladybug.circle(2)
+
+ladybug.hideturtle()
+
+wn = trtl.Screen()
+wn.mainloop()
--- 1-1-6/step5.py
+++ 1-1-6/step5.py
@@ -2,19 +2,46 @@
 import turtle as trtl
 # instead of a descriptive name of the turtle such as painter,
 # a less useful variable name x is used
+
+
+# Create a spider body
 spider = trtl.Turtle()
 spider.pensize(40)
 spider.circle(20)
-legs = 3
-lengthoflegs = 170
-spacing = 380 / legs
+
+# Configures spider legs
+legs = 8
+lengthoflegs = 70
+spacing = 360 / legs - 20
 spider.pensize(5)
+
+# Draw Legs
 n = 0
 while (n < legs):
-  spider.goto(0,0)
-  spider.setheading(spacing * n)
-  spider.forward(lengthoflegs)
-  n = n + 1
+  spider.goto(0, 20)
+  if(n < 4):
+    spider.setheading(spacing * n-45)
+    spider.forward(lengthoflegs)
+
+  else:
+    spider.setheading(spacing * n+45)
+    spider.forward(lengthoflegs)
+  n += 1
+
+#create eyes
+spider.penup()
+spider.goto(-15, 40)
+spider.pendown()
+spider.color("white")
+spider.pensize(4)
+spider.circle(5)
+spider.penup()
+spider.goto(10, 40)
+spider.pendown()
+spider.color("white")
+spider.pensize(4)
+spider.circle(5)
+
 spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
